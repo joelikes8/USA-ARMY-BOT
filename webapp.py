9b7ea207535351af07e8886ad76b8f2e5e7c448a
joelikes8 <johnlikes172@gmail.com>
2025-04-30 21:13:56 -0700

    Update webapp.py for Render deployment

--- webapp.py
+++ webapp.py
@@ -96,4 +96,5 @@ if __name__ == '__main__':
         logger.info("Database tables created.")
     
     # Run the application
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
